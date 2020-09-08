9710ec7bc218c9612c1604069f499007a7f588d7
scattter <413365742@qq.com>
2020-09-08 09:12:41 +0800

    [zk] 修改学员列表为可变数组

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/StudentService.java
@@ -11,45 +11,45 @@ import java.util.List;
 
 @Service
 public class StudentService {
-//    List<Student> students = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
 
-    List<Student> students= Arrays.asList(
-            new Student(1,"沈乐棋"),
-            new Student(2,"徐慧慧"),
-            new Student(3,"陈思聪"),
-            new Student(4,"王江林"),
-            new Student(5,"王登宇"),
-            new Student(6,"杨思雨"),
-            new Student(7,"江雨舟"),
-            new Student(8,"廖燊"),
-            new Student(9,"胡晓"),
-            new Student(10,"但杰"),
-            new Student(11,"盖迈达"),
-            new Student(12,"肖美琦"),
-            new Student(13,"黄云洁"),
-            new Student(14,"齐瑾浩"),
-            new Student(15,"刘亮亮"),
-            new Student(16,"肖逸凡"),
-            new Student(17,"王作文"),
-            new Student(18,"郭瑞凌"),
-            new Student(19,"李明豪"),
-            new Student(20,"党泽"),
-            new Student(21,"肖伊佐"),
-            new Student(22,"贠晨曦"),
-            new Student(23,"李康宁"),
-            new Student(24,"马庆"),
-            new Student(25,"商婕"),
-            new Student(26,"余榕"),
-            new Student(27,"谌哲"),
-            new Student(28,"董翔锐"),
-            new Student(29,"陈泰宇"),
-            new Student(30,"赵允齐"),
-            new Student(31,"张柯"),
-            new Student(32,"廖文强"),
-            new Student(33,"刘轲"),
-            new Student(34,"廖浚斌"),
-            new Student(35,"凌凤仪")
-    );
+    {
+        students.add(new Student(1, "沈乐棋"));
+        students.add(new Student(2, "徐慧慧"));
+        students.add(new Student(3, "陈思聪"));
+        students.add(new Student(4, "王江林"));
+        students.add(new Student(5, "王登宇"));
+        students.add(new Student(6, "杨思雨"));
+        students.add(new Student(7, "江雨舟"));
+        students.add(new Student(8, "廖燊"));
+        students.add(new Student(9, "胡晓"));
+        students.add(new Student(10, "但杰"));
+        students.add(new Student(11, "盖迈达"));
+        students.add(new Student(12, "肖美琦"));
+        students.add(new Student(13, "黄云洁"));
+        students.add(new Student(14, "齐瑾浩"));
+        students.add(new Student(15, "刘亮亮"));
+        students.add(new Student(16, "肖逸凡"));
+        students.add(new Student(17, "王作文"));
+        students.add(new Student(18, "郭瑞凌"));
+        students.add(new Student(19, "李明豪"));
+        students.add(new Student(20, "党泽"));
+        students.add(new Student(21, "肖伊佐"));
+        students.add(new Student(22, "贠晨曦"));
+        students.add(new Student(23, "李康宁"));
+        students.add(new Student(24, "马庆"));
+        students.add(new Student(25, "商婕"));
+        students.add(new Student(26, "余榕"));
+        students.add(new Student(27, "谌哲"));
+        students.add(new Student(28, "董翔锐"));
+        students.add(new Student(29, "陈泰宇"));
+        students.add(new Student(30, "赵允齐"));
+        students.add(new Student(31, "张柯"));
+        students.add(new Student(32, "廖文强"));
+        students.add(new Student(33, "刘轲"));
+        students.add(new Student(34, "廖浚斌"));
+        students.add(new Student(35, "凌凤仪"));
+    }
 
     public List<Student> getAllStudent() {
         return students;
@@ -62,7 +62,9 @@ public class StudentService {
     }
 
     public List<Student> sortStudent(){
-        Collections.shuffle(students);
-        return students;
+        List<Student> newStudents = new ArrayList<Student>();
+        newStudents.addAll(students);
+        Collections.shuffle(newStudents);
+        return newStudents;
     }
 }
